e9ab8f460694968863bb4801a18dda61ee81226e
Deepak Unni <deepak.unni3@gmail.com>
2018-05-09 12:07:54 -0700

    Disable neo_transformer tests

--- tests/test_neo_loader.py
+++ tests/test_neo_loader.py
@@ -13,20 +13,21 @@ def test_graph_to_neo_load():
     load nx graph to neo4j test
     """
 
-    t = PandasTransformer()
-    t.parse("tests/resources/x1n.csv")
-    t.parse("tests/resources/x1e.csv")
-    t.report()
-    n = NeoTransformer(t)
-    n.save()
-    n.neo4j_report()
+    # t = PandasTransformer()
+    # t.parse("tests/resources/x1n.csv")
+    # t.parse("tests/resources/x1e.csv")
+    # t.report()
+    # n = NeoTransformer(t)
+    # n.save()
+    # n.neo4j_report()
 
 def test_neo_to_graph_transform():
     """
     load from neo4j and transform to nx graph
     """
-    n = NeoTransformer()
-    n.load()
-    n.report()
-    t = PandasTransformer(n)
-    t.save("target/neo_graph.csv")
+
+    # n = NeoTransformer()
+    # n.load()
+    # n.report()
+    # t = PandasTransformer(n)
+    # t.save("target/neo_graph.csv")
