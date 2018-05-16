47d1b866403b266e1f5933938ec5491bdc6f89db
cmungall <cjm@berkeleybop.org>
2018-05-16 15:33:21 -0700

    test to see how long relabeling in networkx would take see #8

---
+++ tests/test_random.py
@@ -0,0 +1,45 @@
+from kgx import PandasTransformer
+from kgx import ObanRdfTransformer
+import networkx as nx
+from random import random
+import logging
+
+def test_load():
+    """
+    create a random graph and save it in different formats
+    """
+    G = nx.MultiDiGraph()
+    
+    # Adjust this to test for scalability
+    N = 1000
+    E = N * 3
+    for i in range(1,N):
+        G.add_node(curie(i), label="node {}".format(i))
+    for i in range(1,E):
+        s = random_curie(N)
+        o = random_curie(N)
+        G.add_edge(o,s)
+    print('Nodes={}'.format(len(G.nodes())))
+    rename_all(G)
+    print("Saving tsv")
+    w = PandasTransformer(graph=G)
+    w.save("target/random.tar")
+    print("Saving ttl")
+    w = ObanRdfTransformer(graph=G)
+    w.save("target/random.ttl")
+
+def rename_all(G):
+    m = {}
+    for nid in G.nodes_iter():
+        tid = nid.replace("X:","FOO:")
+        m[nid] = tid
+    print("Renaming...")
+    nx.relabel_nodes(G,m, copy=False)
+    print("Renamed!")
+    
+    
+def random_curie(N):
+    return curie(int(random()*N))
+
+def curie(n):
+    return "X:{}".format(n)
