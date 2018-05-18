4615b75715f67b65a591a3b8711ab9c0a23b87a3
Deepak Unni <deepak.unni3@gmail.com>
2018-05-18 14:16:08 -0700

    Simplify arguments for Transformer class to just graph

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -9,5 +9,5 @@ def test_load():
     t.parse("tests/resources/x1e.csv")
     t.report()
     t.save('target/x1copy.csv')
-    w = GraphMLTransformer(t)
+    w = GraphMLTransformer(t.graph)
     w.save("target/x1n.graphml")
