040f42208ed9756dffd6ca397192c7c6465a74c6
Deepak Unni <deepak.unni3@gmail.com>
2018-06-07 14:31:01 -0700

    Add methods for remapping node and edge properties

--- kgx/transformer.py
+++ kgx/transformer.py
@@ -70,6 +70,82 @@ class Transformer(object):
         graphs.insert(0, self.graph)
         self.graph = nx.compose_all(graphs, "mergedMultiDiGraph")
 
+    def remap_node_identifier(self, new_property):
+        """
+        Remap node `id` attribute with value from node `new_property` attribute
+
+        Parameters
+        ----------
+        new_property: string
+            new property name from which the value is pulled from
+
+        """
+        mapping = {}
+        for node_id in self.graph.nodes_iter():
+            node = self.graph.node[node_id]
+            if new_property in node:
+                mapping[node_id] = node[new_property]
+            else:
+                mapping[node_id] = node_id
+        nx.relabel_nodes(self.graph, mapping, copy=False)
+
+        # update 'subject' of all outgoing edges
+        updated_subject_values = {}
+        for edge in self.graph.out_edges(keys=True):
+            updated_subject_values[edge] = edge[0]
+        nx.set_edge_attributes(self.graph, values = updated_subject_values, name = 'subject')
+
+        # update 'object' of all incoming edges
+        updated_object_values = {}
+        for edge in self.graph.in_edges(keys=True):
+            updated_object_values[edge] = edge[1]
+        nx.set_edge_attributes(self.graph, values = updated_object_values, name = 'object')
+
+    def remap_node_property(self, old_property, new_property):
+        """
+        Remap the value in node `old_property` attribute with value from node `new_property` attribute
+
+        Parameters
+        ----------
+        old_property: string
+            old property name whose value needs to be replaced
+
+        new_property: string
+            new property name from which the value is pulled from
+
+        """
+        mapping = {}
+        for node_id in self.graph.nodes_iter():
+            node = self.graph.node[node_id]
+            if new_property in node:
+                mapping[node_id] = node[new_property]
+            elif old_property in node:
+                mapping[node_id] = node[old_property]
+        nx.set_node_attributes(self.graph, values = mapping, name = old_property)
+
+    def remap_edge_property(self, old_property, new_property):
+        """
+        Remap the value in edge `old_property` attribute with value from edge `new_property` attribute
+
+        Parameters
+        ----------
+        old_property: string
+            old property name whose value needs to be replaced
+
+        new_property: string
+            new property name from which the value is pulled from
+
+        """
+        mapping = {}
+        for edge in self.graph.edges_iter(data=True, keys=True):
+            edge_key = edge[0:3]
+            edge_data = edge[3]
+            if new_property in edge_data:
+                mapping[edge_key] = edge_data[new_property]
+            else:
+                mapping[edge_key] = edge_data[old_property]
+        nx.set_edge_attributes(self.graph, values = mapping, name = old_property)
+
     @staticmethod
     def dump(G):
         """
