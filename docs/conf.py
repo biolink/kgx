c39b104be7a3af192c489ce0a22d6c8015b53749
Lance Hannestad <lhannest@sfu.ca>
2018-06-11 10:34:06 -0700

    Added support for markdown in config.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,12 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
+
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
 
 # The master toctree document.
 master_doc = 'index'
@@ -167,6 +171,3 @@ texinfo_documents = [
      author, 'KnowledgeGraphExchange', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
