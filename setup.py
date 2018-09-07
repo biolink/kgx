6afee7b84acdb8eb6a42b46dd31355610bfd0e20
Lance Hannestad <lhannest@sfu.ca>
2018-09-07 10:40:38 -0700

    Implemented validator using BiolinkMG - Checks that every category and edge label is in the biolink model - Checks the domain and range of edges - Checks that all required properties exist
    - Fixed absent requirements in setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ requires = [
     "click==6.7",
     "neo4j-driver>=1.5.3",
     "pyyaml>=0.0",
-    "Click"]
+    "neo4jrestclient",
+    "prologterms",
+    "Click",
+    "BiolinkMG"
+]
 
 setup(
     name='Knowledge Graph Exchange',
