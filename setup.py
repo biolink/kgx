e1d3df392cf94facbf6dcbb8c24d6078ef4eac01
Richard Bruskiewich <richard@starinformatics.com>
2018-11-09 19:16:56 +0000

    Changed bound on version

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 requires = [
     "prefixcommons>=0.1.4",
-    "networkx==1.11",
-    "SPARQLWrapper==1.8.0",
+    "networkx>=1.11",
+    "SPARQLWrapper>=1.8.0",
     "pandas<0.21",
     "pytest>=0.0",
     "mypy>=0.0",
     "pystache>=0.0",
     "pytest_logging>=0.0",
     "rdflib>=0.0",
-    "click==6.7",
+    "click>=6.7",
     "neo4j-driver>=1.5.3",
     "pyyaml>=0.0",
     "neo4jrestclient",
