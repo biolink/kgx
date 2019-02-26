e172d682e8160450e14577e05075cbcac9e90a1b
Lance Hannestad <lhannest@gmail.com>
2019-02-26 16:41:55 +0000

    Fixed scripts/main.py

--- examples/monarch-build-workflow/scripts/main.py
+++ examples/monarch-build-workflow/scripts/main.py
@@ -5,10 +5,10 @@ nodes.csv files, into a single NetworkX graph, and performs `clique_merge` on it
 Finally, saves the resulting NetworkX graph as `clique_merged.csv`
 """
 
-from kgx import ObanRdfTransformer2, JsonTransformer, HgncRdfTransformer, RdfOwlTransformer
+from kgx import ObanRdfTransformer2, JsonTransformer, HgncRdfTransformer, RdfOwlTransformer2
 from kgx import clique_merge
 
-t = RdfOwlTransformer()
+t = RdfOwlTransformer2()
 t.parse('data/mondo.owl')
 t = JsonTransformer(t)
 t.save('results/mondo.json')
@@ -47,15 +47,16 @@ t = JsonTransformer(t)
 t.save('results/clinvar.json')
 
 t = JsonTransformer()
+t.parse('results/mondo.json')
 t.parse('results/hgnc.json')
 t.parse('results/clinvar.json')
 t.parse('results/omim.json')
 t.parse('results/hpoa.json')
 t.parse('results/orphanet.json')
 
-t = PandasTransformer(t.graph)
-t.parse('data/semmeddb_edges.csv')
-t.parse('data/semmeddb_nodes.csv')
+#t = PandasTransformer(t.graph)
+#t.parse('data/semmeddb_edges.csv')
+#t.parse('data/semmeddb_nodes.csv')
 
 t.graph = clique_merge(t.graph)
-t.save('results/clique_merged.csv')
+t.save('results/clique_merged.json')
