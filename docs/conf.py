52cc2c86b218319a83921241e71cb05f00744bae
Deepak Unni <deepak.unni3@gmail.com>
2020-01-02 16:11:26 -0800

    set up path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../kgx'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- General configuration ------------------------------------------------
