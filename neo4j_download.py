07c81c4879d3c9387dc237c511a7566f5749f6d7
gregr <greg.weiqi@gmail.com>
2020-04-20 12:29:57 -0400

    Default to TSV format in neo4j_download.py

--- neo4j_download.py
+++ neo4j_download.py
@@ -1,13 +1,13 @@
 import logging, yaml
-from kgx import CsvSink, ProgressSink, NeoSource
+from kgx import CsvSink, TsvSink, ProgressSink, NeoSource
 
 logging.basicConfig(level=logging.INFO)
 
-with open ('config.yml', 'r') as ymlfile:
+with open('config.yml', 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
     outputname = cfg['neo4j']['outputname']
 
-with CsvSink(outputname) as csv_sink:
-    psink = ProgressSink(csv_sink)
+with TsvSink(outputname) as sink:
+    psink = ProgressSink(sink)
     neo_src = NeoSource(psink)
     neo_src.load()
