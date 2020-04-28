cdc6d8b00c8ac737f11f485fbb681e3432188fe3
Deepak Unni <deepak.unni3@gmail.com>
2020-04-28 08:19:38 -0700

    Define location for JSON-LD context in config.yml
    And allow for a local JSON-LD context and rely
    on remote download only when there is no local version

--- kgx/config.py
+++ kgx/config.py
@@ -1,5 +1,7 @@
 from os import path
+import json
 
+import requests
 import yaml
 
 config = None
@@ -25,3 +27,27 @@ def get_config(filename: str = CONFIG_FILENAME) -> dict:
     if config is None:
         config = yaml.load(open(filename), Loader=yaml.FullLoader)
     return config
+
+def get_jsonld_context(name: str = "biolink"):
+    """
+    Get contents of a JSON-LD context.
+
+    Returns
+    -------
+    dict
+        the contents of the JSON-LD context
+
+    """
+    content = None
+    url = config['jsonld-context'][name]['local']
+    if path.exists(url):
+        content = json.load(open(url))
+    else:
+        try:
+            url = config['jsonld-context'][name]['remote']
+            content = requests.get(url).json()
+        except ConnectionError:
+            raise Exception(f'Unable to download JSON-LD context from {url}')
+    if '@context' in content:
+        content = content['@context']
+    return content
