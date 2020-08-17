d9bc66645ff337500903c9d8d5bd0229323fbe17
Deepak Unni <deepak.unni3@gmail.com>
2020-08-17 15:23:52 -0700

    Update tests

--- tests/unit/test_rdf_owl_transformer.py
+++ tests/unit/test_rdf_owl_transformer.py
@@ -10,9 +10,50 @@ resource_dir = os.path.join(cwd, '../resources')
 target_dir = os.path.join(cwd, '../target')
 
 
-def test_owl_load():
+def test_owl_parse1():
     t = RdfOwlTransformer()
     t.parse(os.path.join(resource_dir, 'goslim_generic.owl'))
+
+    n1 = t.graph.nodes['GO:0008150']
+    assert n1['name'] == 'biological_process'
+    assert 'synonym' in n1 and 'biological process' in n1['synonym']
+    assert 'description' in n1
+    assert 'comment' in n1
+    assert 'hasAlternativeId' in n1 and 'GO:0044699' in n1['hasAlternativeId']
+
+    n2 = t.graph.nodes['GO:0003674']
+    n2['name'] = 'molecular_function'
+    assert 'synonym' in n2 and 'molecular function' in n2['synonym']
+    assert 'description' in n2
+    assert 'comment' in n2
+    assert 'hasAlternativeId' in n2 and 'GO:0005554' in n2['hasAlternativeId']
+
+    n3 = t.graph.nodes['GO:0005575']
+    n3['name'] = 'cellular_component'
+    assert 'synonym' in n3 and 'cellular component' in n3['synonym']
+    assert 'description' in n3
+    assert 'comment' in n3
+    assert 'hasAlternativeId' in n3 and 'GO:0008372' in n3['hasAlternativeId']
+
+    e1 = list(t.graph.get_edge_data('GO:0008289', 'GO:0003674').values())[0]
+    assert e1['subject'] == 'GO:0008289'
+    assert e1['edge_label'] == 'biolink:subclass_of'
+    assert e1['object'] == 'GO:0003674'
+    assert e1['relation'] == 'rdfs:subClassOf'
+
+
+def test_owl_parse2():
+    np = {
+        'http://www.geneontology.org/formats/oboInOwl#inSubset',
+    }
+    predicate_mapping = {
+        'http://www.geneontology.org/formats/oboInOwl#inSubset': 'subsets',
+        'http://www.geneontology.org/formats/oboInOwl#hasOBONamespace': 'namespace',
+        'http://www.geneontology.org/formats/oboInOwl#hasAlternativeId': 'xrefs',
+    }
+    t = RdfOwlTransformer()
+    t.set_predicate_mapping(predicate_mapping)
+    t.parse(os.path.join(resource_dir, 'goslim_generic.owl'), node_property_predicates=np)
 
     n1 = t.graph.nodes['GO:0008150']
     assert n1['name'] == 'biological_process'
