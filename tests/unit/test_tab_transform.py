da7304b6e78ed3eedb630c3e8aa9c1539d2f087d
Deepak Unni <deepak.unni3@gmail.com>
2021-01-28 17:43:00 -0800

    Update tests

--- tests/unit/test_tab_transform.py
+++ tests/unit/test_tab_transform.py
@@ -1,16 +1,13 @@
 import os
 
-import pytest
-
 from kgx.sink.json_sink import JsonSink
-from kgx.sink.nx_graph_sink import NxGraphSink
+from kgx.sink.graph_sink import GraphSink
 from kgx.source.json_source import JsonSource
-from kgx.source.nx_graph_source import NxGraphSource
+from kgx.source.graph_source import GraphSource
 from kgx.source.tsv_source import TsvSource
-from kgx.sink.tab_sink import TsvSink
-from kgx.stream.stream import Stream
-from kgx.transformers.configurable_transformer import ConfigurableTransformer, ConfigurableTransformer2, \
-    ConfigurableTransformer3
+from kgx.sink.tsv_sink import TsvSink
+from kgx.stream import Stream
+from kgx.new_transformer import Transformer
 from tests import print_graph
 
 cwd = os.path.abspath(os.path.dirname(__file__))
@@ -46,6 +43,7 @@ def test_json_transform():
     sink = JsonSink(
         filename=os.path.join(target_dir, 'my_graph.json'),
         format='json',
+        compression='gz'
     )
     source = TsvSource()
     g = source.parse(
@@ -63,14 +61,14 @@ def test_json_transform():
 
 def test_nx_transform():
     input_channel = Stream()
-    intermediate_sink = NxGraphSink()
+    intermediate_sink = GraphSink()
     input_source = TsvSource()
     input_source_generator = input_source.parse(os.path.join(resource_dir, 'graph_nodes.tsv'), 'tsv')
     input_channel.process(input_source_generator, intermediate_sink)
     input_source_generator = input_source.parse(os.path.join(resource_dir, 'graph_edges.tsv'), 'tsv')
     input_channel.process(input_source_generator, intermediate_sink)
 
-    intermediate_source = NxGraphSource()
+    intermediate_source = GraphSource()
     intermediate_source_generator = intermediate_source.parse(intermediate_sink.graph)
 
     output_sink = TsvSink(os.path.join(target_dir, 'my_graph'), 'tsv', None)
@@ -83,42 +81,54 @@ def test_configurable_transformer1():
         os.path.join(resource_dir, 'graph_nodes.tsv'),
        os.path.join(resource_dir, 'graph_edges.tsv')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.transform(f, 'tsv')
-    print_graph(cct.store.graph)
+        input_args = {'filename': f, 'format': 'tsv'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+
 
 def test_configurable_transformer2():
     input_files = [
         os.path.join(resource_dir, 'graph_nodes.tsv'),
        os.path.join(resource_dir, 'graph_edges.tsv')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'tsv')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'tsv')
+        input_args = {'filename': f, 'format': 'tsv'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': os.path.join(target_dir, 'my_new_graph'), 'format': 'tsv'}
+    # TODO: empty output due to node_properties and edge_properties being empty
+    t.save(output_args)
+
 
 def test_configurable_transformer3():
     input_files = [
         os.path.join(resource_dir, 'cm_nodes.csv'),
        os.path.join(resource_dir, 'cm_edges.csv')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'csv')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'tsv')
+        input_args = {'filename': f, 'format': 'csv'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': os.path.join(target_dir, 'my_new_graph'), 'format': 'tsv'}
+    t.save(output_args)
+
 
 def test_configurable_transformer4():
     input_files = [
         os.path.join(resource_dir, 'test.tar.gz'),
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'tsv', 'tar.gz')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'tsv')
+        input_args = {'filename': f, 'format': 'tsv', 'compression': 'tar.gz'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_new_graph', 'format': 'tsv'}
+    t.save(output_args)
+
 
 def test_json_source():
     input_files = [
@@ -130,17 +140,18 @@ def test_json_source():
             print(r)
 
 
-@pytest.mark.skip()
 def test_configurable_transformer5():
     # Tough to implement a stream export of JSON
     input_files = [
         os.path.join(resource_dir, 'valid.json'),
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'json')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'json')
+        input_args = {'filename': f, 'format': 'json'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_new_graph', 'format': 'json'}
+    t.save(output_args)
 
 
 def test_configurable_transformer6():
@@ -148,79 +159,98 @@ def test_configurable_transformer6():
         os.path.join(resource_dir, 'valid_nodes.jsonl'),
         os.path.join(resource_dir, 'valid_edges.jsonl')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'jsonl')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'jsonl')
+        input_args = {'filename': f, 'format': 'jsonl'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_new_graph', 'format': 'jsonl'}
+    t.save(output_args)
+
 
 def test_configurable_transformer7():
     input_files = [
         os.path.join(resource_dir, 'valid_nodes.jsonl'),
         os.path.join(resource_dir, 'valid_edges.jsonl')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'jsonl')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'jsonl', compression='gz')
+        input_args = {'filename': f, 'format': 'jsonl'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_new_graph', 'format': 'jsonl', 'compression': 'gz'}
+    t.save(output_args)
 
 
 def test_configurable_transformer8():
     input_files = [
         os.path.join(resource_dir, 'goslim_generic.json')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'obojson')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'jsonl')
+        input_args = {'filename': f, 'format': 'obojson'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_new_graph', 'format': 'jsonl'}
+    t.save(output_args)
 
 
 def test_configurable_transformer9():
     input_files = [
         os.path.join(resource_dir, 'rsa_sample.json')
     ]
-    cct = ConfigurableTransformer()
+    t = Transformer()
     for f in input_files:
-        cct.load(f, 'trapi-json')
-    print_graph(cct.store.graph)
-    cct.save('my_new_graph', 'jsonl')
+        input_args = {'filename': f, 'format': 'trapi-json'}
+        t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_new_graph', 'format': 'jsonl'}
+    t.save(output_args)
 
 
 def test_configurable_transformer10():
-    cct = ConfigurableTransformer2()
-    cct.load('http://localhost:9000', 'neo4j', 'admin')
-    print_graph(cct.store.graph)
-    cct.save('my_chebi_graph', 'jsonl')
+    t = Transformer()
+    input_args = {'format': 'neo4j', 'uri': 'http://localhost:9000', 'username': 'neo4j', 'password': 'admin'}
+    t.transform(input_args)
+    print_graph(t.store.graph)
+    output_args = {'filename': 'my_chebi_graph', 'format': 'jsonl'}
+    t.save(output_args)
 
 
 def test_configurable_transformer11():
-    cct = ConfigurableTransformer2(stream=True)
-    cct.transform('http://localhost:9000', 'neo4j', 'admin', 'my-streamed-chebi-graph', 'jsonl')
+    t = Transformer(stream=True)
+    input_args = {'format': 'neo4j', 'uri': 'http://localhost:9000', 'username': 'neo4j', 'password': 'admin'}
+    output_args = {'filename': 'my-streamed-chebi-graph', 'format': 'jsonl'}
+    t.transform(input_args, output_args)
 
 
 def test_configurable_transformer12():
-    cct = ConfigurableTransformer3(stream=False)
-    cct.transform('http://localhost:9000', 'neo4j', 'admin', 'http://localhost:8484', 'neo4j', 'test')
+    t = Transformer(stream=False)
+    input_args = {'format': 'neo4j', 'uri': 'http://localhost:9000', 'username': 'neo4j', 'password': 'admin'}
+    output_args = {'format': 'neo4j', 'uri': 'http://localhost:8484', 'username': 'neo4j', 'password': 'test'}
+    t.transform(input_args, output_args)
     #print_graph(cct.store.graph)
 
 
 def test_configurable_transformer13():
-    cct = ConfigurableTransformer3(stream=True)
-    cct.transform('http://localhost:9000', 'neo4j', 'admin', 'http://localhost:8484', 'neo4j', 'test')
+    t = Transformer(stream=True)
+    input_args = {'format': 'neo4j', 'uri': 'http://localhost:9000', 'username': 'neo4j', 'password': 'admin'}
+    output_args = {'format': 'neo4j', 'uri': 'http://localhost:8484', 'username': 'neo4j', 'password': 'test'}
+    t.transform(input_args, output_args)
 
 
 def test_configurable_transformer14():
-    cct = ConfigurableTransformer(stream=True)
-    #cct.transform(os.path.join(resource_dir, '../../','orphanet-export.sorted.nt'), 'nt', None, 'my_orpha_graph', 'jsonl')
-    cct.transform(os.path.join(resource_dir, '../../', 'chebi_sorted.nt'), 'nt', None, 'my_orpha_graph',
-                  'jsonl')
+    t = Transformer(stream=True)
+    input_args = {'filename': os.path.join(resource_dir, '../../', 'chebi_sorted.nt'), 'format': 'nt'}
+    output_args = {'filename': 'my_orpha_graph.nt', 'format': 'jsonl'}
+    t.transform(input_args, output_args)
 
 
 def test_configurable_transformer15():
-    cct = ConfigurableTransformer(stream=True)
-    #cct.transform(os.path.join(resource_dir, '../../', 'chebi_sorted.nt'), 'nt', None, 'my_chebi_graph', 'nt')
-    cct.transform(os.path.join(resource_dir, '../../', 'orphanet-export.sorted.nt'), 'nt', None, 'my_orpha_graph.nt', 'nt')
+    t = Transformer(stream=True)
+    input_args = {'filename': os.path.join(resource_dir, '../../', 'orphanet-export.sorted.nt'), 'format': 'nt'}
+    output_args = {'filename': 'my_orpha_graph.nt', 'format': 'nt'}
+    t.transform(input_args, output_args)
+
 
 
