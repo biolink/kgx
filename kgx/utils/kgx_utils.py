4c51604331c5643f02c8526ef69ce916d240264a
Richard Bruskiewich <richard@starinformatics.com>
2021-07-01 17:14:40 -0700

    Use BMT default schema  instead of local KGX config

--- kgx/utils/kgx_utils.py
+++ kgx/utils/kgx_utils.py
@@ -264,10 +264,12 @@ def get_toolkit(schema: Optional[str] = None) -> Toolkit:
     """
     global toolkit
     if toolkit is None:
-        if not schema:
-            config = get_config()
-            schema = config['biolink-model']
-        toolkit = Toolkit(schema=schema)
+        # We no longer assume that the KGX config containsthe default schema
+        # but rather  now, defer schema resolution to BMT
+        # if not schema:
+        #     config = get_config()
+        #     schema = config['biolink-model']
+        toolkit = Toolkit(schema=schema) if schema else Toolkit()
     return toolkit
 
 
