0a0002eb7988f2aeaa2d27dc6687bbf5350d0de0
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-04-25 17:26:01 -0700

    working test

--- tests/unit/test_source/test_obograph_source.py
+++ tests/unit/test_source/test_obograph_source.py
@@ -16,8 +16,9 @@ def test_read_hp_obograph1():
     )
     categories = []
     for rec in g:
-        if rec and type(rec[1]) != 'str' and rec[1].get("category") not in categories:
-            categories.append(rec[1].get("category"))
+        if rec:
+            print(rec[1].get("name"))
+            print(rec[1].get("category"))
     print(categories)
 
 
