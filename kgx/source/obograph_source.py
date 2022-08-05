e3f04eba5d3608271f066c9c09b7eb7abf18a35e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-08-04 17:36:55 -0700

    fix node provenance, not edge

--- kgx/source/obograph_source.py
+++ kgx/source/obograph_source.py
@@ -56,16 +56,17 @@ class ObographSource(JsonSource):
 
         """
 
-        print(kwargs)
+        if "provided_by" not in kwargs:
+            if "name" in kwargs:
+                self.set_provenance_map({"provided_by": kwargs["name"]})
+            else:
+                print(kwargs)
+                self.set_provenance_map({"provided_by": filename})
         n = self.read_nodes(filename, compression)
         # nodes don't get the entirety of the knowledge_source association_slot properties.
-        # this -self.set_provenance_map(kwargs)- will give nodes, edge provenance
-        if "provided_by" in kwargs:
-            self.set_provenance_map({'provided_by': kwargs['provided_by'],
-                                    'default_provenance': kwargs['default_provenance']})
-        else:
-            print(kwargs)
-            self.set_provenance_map({'default_provenance': kwargs['default_provenance']})
+        # self.set_provenance_map(kwargs)
+        # TODO: figure out how to handle the edges provenance
+        # right now they are not returning any provenance
         e = self.read_edges(filename, compression)
         yield from chain(n, e)
 
