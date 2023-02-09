66a8e776e7ea6d3a6b3b1be0f86facc59d4554f2
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-02-09 09:20:53 -0800

    fix version test

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -5,12 +5,12 @@ from kgx.config import get_biolink_model_schema
 
 def test_valid_biolink_version():
     try:
-        schema = get_biolink_model_schema("3.1.2")
+        schema = get_biolink_model_schema("v3.2.0")
     except TypeError as te:
         assert False, "test failure!"
     assert (
         schema
-        == "https://raw.githubusercontent.com/biolink/biolink-model/3.1.2/biolink-model.yaml"
+        == "https://raw.githubusercontent.com/biolink/biolink-model/v3.2.0/biolink-model.yaml"
     )
 
 
