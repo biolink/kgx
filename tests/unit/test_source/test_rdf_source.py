c7308b54d74aee659fd229d2621e61ea1930d58c
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-02-09 17:03:14 -0800

    remove unused print statements

--- tests/unit/test_source/test_rdf_source.py
+++ tests/unit/test_source/test_rdf_source.py
@@ -18,8 +18,6 @@ def test_read_nt1():
 
     g = s.parse(os.path.join(RESOURCE_DIR, "rdf", "test1.nt"))
     nodes, edges = load_graph_dictionary(g)
-    pprint(nodes)
-    pprint(edges)
     assert len(nodes) == 2
     assert len(edges) == 1
 
