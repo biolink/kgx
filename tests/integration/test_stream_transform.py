4e29e882fbe0abb308be01ce255775adcc2ba61c
Kevin Schaper <kevinschaper@gmail.com>
2023-04-07 14:36:50 -0700

    Update counts to reflect reification difference

--- tests/integration/test_stream_transform.py
+++ tests/integration/test_stream_transform.py
@@ -455,8 +455,8 @@ def test_transform4(query):
                 "format": "owl",
             },
             {"filename": os.path.join(TARGET_DIR, "graph3s5.nt"), "format": "nt"},
-            221,
-            1052,
+            220,
+            1050,
         ),
     ],
 )
